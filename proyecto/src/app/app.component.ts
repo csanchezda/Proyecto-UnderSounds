b45616460b5899912e9abb8cea701ffa6441cb2b
Cristina <cvaqueri@alumnos.unex.es>
2025-03-14 20:09:15 +0100

    Template con header, subheader, y footer

--- proyecto/src/app/app.component.ts
+++ proyecto/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MenuLayoutComponent } from './menu-layout/menu-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [RouterModule, CommonModule, MenuLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mi-Undersounds';
+  showMenuLayout = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      // Ocultar layout en páginas específicas
+      //this.showMenuLayout = !['/login', '/register'].includes(this.router.url); --> Ejemplo de como ocultarlo en páginas
+    });
+  }
 }
