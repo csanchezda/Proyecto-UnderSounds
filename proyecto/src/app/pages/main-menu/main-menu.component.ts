a2fe3ca35b4a12f3f7f9ec833d6bd7aba7b5af96
Cristina <cvaqueri@alumnos.unex.es>
2025-03-15 23:18:40 +0100

    Vista menú principal y ajustes de layout (Template) + Vista Acerca de

---
+++ proyecto/src/app/pages/main-menu/main-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-menu',
+  imports: [CommonModule],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
+})
+export class MainMenuComponent {
+  noticias = [
+    { img: 'assets/images/img1_menu.jpeg', titulo: 'Nuevo Festival de Música', descripcion: 'Un increíble festival underground se celebrará este verano.' },
+    { img: 'assets/images/img2_menu.jpeg', titulo: 'Vinilos en Aumento', descripcion: 'Las ventas de vinilos están alcanzando récords históricos.' },
+    { img: 'assets/images/img3_menu.jpeg', titulo: 'Banda Emergente Rompe Récords', descripcion: 'Una banda indie ha conseguido el #1 en las listas de streaming.' },
+    { img: 'assets/images/img4_menu.jpeg', titulo: 'Gira Mundial de Banda Legendaria', descripcion: 'Anunciada la gira final de una de las bandas más icónicas.' },
+    { img: 'assets/images/img5_menu.jpeg', titulo: 'Streaming para Artistas Underground', descripcion: 'Nueva plataforma para artistas independientes revoluciona la industria.' },
+    { img: 'assets/images/img6_menu.jpeg', titulo: 'Explosión del Rock Indie', descripcion: 'El rock indie está resurgiendo con nuevos talentos en la escena.' }
+  ];
+}
