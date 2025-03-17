8128140d08d1d23eb955f184edbc8417d6a4a54d
Cristina <cvaqueri@alumnos.unex.es>
2025-03-17 16:39:27 +0100

    box-container + Vista Login + Vista Register + Vista Forgot-Password

---
+++ proyecto/src/app/box-container/box-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-container',
+  imports: [],
+  templateUrl: './box-container.component.html',
+  styleUrl: './box-container.component.css'
+})
+export class BoxContainerComponent {
+
+}
