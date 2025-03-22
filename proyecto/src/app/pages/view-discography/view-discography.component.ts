ce9fabaadc44270c8a8d4721455b99cae49f89cb
Saimon <92518360+asimon02@users.noreply.github.com>
2025-03-22 02:05:46 +0100

    Actualización_Discografía

--- proyecto/src/app/pages/view-discography/view-discography.component.ts
+++ proyecto/src/app/pages/view-discography/view-discography.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-discography',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './view-discography.component.html',
   styleUrl: './view-discography.component.css'
 })
 export class ViewDiscographyComponent {
+  songs: any[] = [];
+  albums: any[] = [];
 
+  ngOnInit(): void {
+    this.loadSongs();
+    this.loadAlbums();
+  }
+
+  loadSongs() {
+    this.songs = [
+      { name: 'Canción 1', image: 'assets/images/artists/artist1.png' },
+      { name: 'Canción 2', image: 'assets/images/artists/artist1.png' },
+      { name: 'Canción 3', image: 'assets/images/artists/artist1.png' },
+      { name: 'Canción 4', image: 'assets/images/artists/artist1.png' }
+    ];
+  }
+
+  loadAlbums() {
+    this.albums = [
+      { name: 'Álbum 1', image: 'assets/images/Square.png', duration: '23:00' },
+      { name: 'Álbum 2', image: 'assets/images/Square.png', duration: '25:00' },
+      { name: 'Álbum 3', image: 'assets/images/Square.png', duration: '31:00' },
+      { name: 'Álbum 4', image: 'assets/images/Square.png', duration: '26:00' }
+    ];
+  }
 }
