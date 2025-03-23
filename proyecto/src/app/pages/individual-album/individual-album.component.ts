5e4294e10bda7afb02feba119a1459accce2def5
Carmen Sanchez <csanchezda@alumnos.unex.es>
2025-03-23 18:52:23 +0100

    albums changes

---
+++ proyecto/src/app/pages/individual-album/individual-album.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-individual-album',
+  imports: [],
+  templateUrl: './individual-album.component.html',
+  styleUrl: './individual-album.component.css'
+})
+export class IndividualAlbumComponent {
+
+}
