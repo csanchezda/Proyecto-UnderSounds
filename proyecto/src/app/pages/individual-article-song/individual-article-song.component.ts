facbac5ef65b6fc204493289f7f97e8933eca464
Carmen Sanchez <csanchezda@alumnos.unex.es>
2025-03-25 00:50:26 +0100

    individual article

---
+++ proyecto/src/app/pages/individual-article-song/individual-article-song.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-individual-article-song',
+  imports: [],
+  templateUrl: './individual-article-song.component.html',
+  styleUrl: './individual-article-song.component.css'
+})
+export class IndividualArticleSongComponent {
+
+}
