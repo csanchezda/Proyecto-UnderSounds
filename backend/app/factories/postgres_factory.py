11e123061a400bea9f3389a327b6472617b665e2
Carmen Sanchez <csanchezda@alumnos.unex.es>
2025-04-21 23:20:52 +0200

    arreglo de los patrones de diseño

--- backend/app/factories/postgres_factory.py
+++ backend/app/factories/postgres_factory.py
@@ -1,6 +1,11 @@
 from app.factories.dao_factory import DAOFactory
 from app.dao.postgres.user_dao_postgres import PostgresUserDAO
+from app.db.database import db_session
 
 class PostgresFactory(DAOFactory):
+    def __init__(self):
+        self._session_context = db_session
+
     def create_user_dao(self):
-        return PostgresUserDAO()
+        # Devuelve el DAO con el context manager de sesión
+        return PostgresUserDAO(self._session_context)
